0b86c044fabff2c6d1f95f22db9b4364d7712a5b
Saurabh Pujari <saurabhpuj99@gmail.com>
2024-04-08 11:57:10 +0530

    formatted doc strings

--- object_tracker/__init__.py
+++ object_tracker/__init__.py
@@ -1,4 +1,38 @@
 """
+The object_tracker package provides classes to track changes to an object's attributes
+
+```
+from object_tracker import TrackerMixin, Tracker
+
+# Eg. 1
+class User(TrackerMixin):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.tracker = Tracker()
+
+user = User(name='Alice', age=30)
+user.name = 'Bob'
+print(user.tracker.has_changed()) # True
+
+
+# Eg. 2
+class MyClass:
+        pass
+    
+obj = MyClass()
+tracker = Tracker(obj)
+obj.attribute = 'new_value'
+print(tracker.has_changed(obj)) # True
+
+
+# Eg. 3
+tracker = Tracker()
+tracker.track('attribute', 'old_value', 'new_value')
+print(tracker.has_attribute_changed('attribute')) # True
+
+```
+
 Copyright (c) Saurabh Pujari
 All rights reserved.
 
